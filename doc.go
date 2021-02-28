ec17c568d63600ea12ad2913dd09fed4f0a37197
Paul R. Tagliamonte <paultag@gmail.com>
2021-02-28 11:09:44 -0500

    Add in doc

---
+++ doc.go
@@ -0,0 +1,29 @@
+// {{{ Copyright (c) Paul R. Tagliamonte <paul@k3xec.com>, 2021
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE. }}}
+
+// Package mjpeg implements support for mjpeg (sometimes M-JPEG) video streams.
+//
+// Each frame of the stream is sent as an encoded JPEG. This, while it has
+// quite a bit of overhead, is simple to work with both from a client and server
+// point of view. Clients update the stream frame-by-frame with a golang
+// image.Image.
+package mjpeg
+
+// vim: foldmethod=marker
